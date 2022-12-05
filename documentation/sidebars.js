07127131cdc948a548b252864884eec67fbc4567
Dr-Electron <dr-electr0n@protonmail.com>
2022-12-05 21:42:58 +0100

    Update sidebars.js

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -12,44 +12,52 @@
 module.exports = {
   docs: [
     {
-      type: "doc",
-      id: "welcome",
-      label: "Welcome",
-    },
-    {
-      type: "link",
-      label: "Getting Started",
-      href: "https://wiki.iota.org/learn/contribute-to-wiki/reference/structure/getting_started"
-    },
-    {
-      type: "link",
-      label: "How Tos",
-      href: "https://wiki.iota.org/learn/contribute-to-wiki/reference/structure/how_tos"
-    },
-    {
-      type: "link",
-      label: "Tutorials",
-      href: "https://wiki.iota.org/learn/contribute-to-wiki/reference/structure/tutorials"
-    },
-    {
-      type: "link",
-      label: "Explanations",
-      href: "https://wiki.iota.org/learn/contribute-to-wiki/reference/structure/explanations",
-    },
-    {
-      type: "link",
-      label: "Reference",
-      href: "https://wiki.iota.org/learn/contribute-to-wiki/reference/structure/reference"
-    },
-    {
-      type: "doc",
-      id: "troubleshooting",
-      label: "Troubleshooting",
-    },
-    {
-      type: "doc",
-      id: "contribute",
-      label: "Contribute",
-    },
+      type: "category",
+      label: "Template",
+      link: {type: 'doc', id: 'welcome'},
+      collapsed: false,
+      items: [
+        {
+          type: "doc",
+          id: "welcome",
+          label: "Welcome",
+        },
+        {
+          type: "link",
+          label: "Getting Started",
+          href: "https://wiki.iota.org/learn/contribute-to-wiki/reference/structure/getting_started"
+        },
+        {
+          type: "link",
+          label: "How Tos",
+          href: "https://wiki.iota.org/learn/contribute-to-wiki/reference/structure/how_tos"
+        },
+        {
+          type: "link",
+          label: "Tutorials",
+          href: "https://wiki.iota.org/learn/contribute-to-wiki/reference/structure/tutorials"
+        },
+        {
+          type: "link",
+          label: "Explanations",
+          href: "https://wiki.iota.org/learn/contribute-to-wiki/reference/structure/explanations",
+        },
+        {
+          type: "link",
+          label: "Reference",
+          href: "https://wiki.iota.org/learn/contribute-to-wiki/reference/structure/reference"
+        },
+        {
+          type: "doc",
+          id: "troubleshooting",
+          label: "Troubleshooting",
+        },
+        {
+          type: "doc",
+          id: "contribute",
+          label: "Contribute",
+        },
+      ]
+    }
   ],
 };
